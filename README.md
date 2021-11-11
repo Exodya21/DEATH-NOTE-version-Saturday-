e3a186b82e8529b8b47dea515d74f5ef211dd6c0
Jaume <84014939+Exodya21@users.noreply.github.com>
2021-11-11 10:15:11 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,71 @@
-# DEATH-NOTE-version-Saturday-
+# Fortune-Wheel death Note version
+
+<a href='https://docs.google.com/presentation/d/1CZ9coOZDxAytMdcq8-6hDCn5i_Q_O7CwObKZ-uvWFCY/edit?usp=sharing'>Presentacion Google Slides</a>
+
+<a href='https://fortune-wheel-pinguin.github.io/FortuneWheelV2.0/'>Git Pages</a>
+
+Se trata de un proyecto en equipo que realiazamos en el Bootcamp de Factoria F5. Trabajamos en una app llamada <a href="https://github.com/Fortune-Wheel-pinguin/FortuneWheelV2.0">Fortune-Wheel</a>(Este link te redirecciona a nuestra app) en la cual creamos un sistema de selección de sacrificios de manera justa y aleatoria para los miembros. Una vez un nombre sale en la wheel, no puede volver a aparecer hasta que la wheel of doom se vuelva a iniciar.
+
+Comenzando
+Para tener una copia de nuestro projecto debes de tener unos programas que a continuacion te vamos a mostar.
+
+Pre-requisitos 
+
+Necestitamos: <a href='https://code.visualstudio.com/'>Visual Studio Code</a> y <a href='https://github.com/'>GitHub</a>
+
+Que es Visual Studio Code y GitHub???
+<a href='https://code.visualstudio.com/'>Visual Studio Code</a> es un editor de código fuente. Es compatible con varios lenguajes de programación y un conjunto de características que pueden o no estar disponibles para un lenguaje dado.
+
+
+
+
+
+Instalación
+Te dejaremos en la etiqueta tutoriales unos videos para poder descargar e instalar cada programa. Ademas de un video de como configurarlos para que esten vinculados.
+
+Tutoriales 
+Instalacion de Visual Studio Code: <a href='https://code.visualstudio.com/'>Visual Studio Code</a>
+
+Instalacion de GitHub: <a href='https://github.com/'>GitHub</a>
+
+Vinculacion de ambos: <a href='https://www.youtube.com/watch?v=htstKtlFKeE'>Vinculacion</a>
+
+
+
+
+
+Construido con :
+
+Trello - Kanban o gestor de proyectos
+
+Figma - Programa para diseñar el proyecto (Wireframe y Mockup)
+
+Flaticon - Bancos de iconos
+
+Visual Studio Code - Editor de codigo
+
+GitHub - Repositorio online
+
+
+
+
+Autores
+-Integrantes y roles dentro del proyecto:
+
+-Felicia - Scrum Team - <a href='https://github.com/timfel20'>timfel20</a>
+
+-Esther - Scrum Team - <a href='https://github.com/estherc2014
+'>estherc2014</a>
+
+-Jaume Gomicia - Scrum Master - <a href='https://github.com/Exodya21'>Exodya21</a>
+
+-Jura - Product Owner - <a href='https://github.com/eljura111'>eljura111</a>
+
+-David - Scrum Team - <a href='https://github.com/Davaious'>Davaious</a>
+
+Expresiones de Gratitud
+Comenta a otros sobre este proyecto
+Invita una cerveza :cerveza: o un café :café: a alguien del equipo.
+Da las gracias a Factoria F5 por darnos la oportunidad de poder demostrar de lo que somos capaces ( ͡:fuego: ͜ʖ ͡:fuego:).
+Agraddecer tambien a nuestros formadores Diego Cordoba y Sergi Virgili Flor por tener tanta paciencia y por brindarnos todo su conocimiento.
+Etc.
---
+++ js/main.js
@@ -0,0 +1,165 @@
+let names = ['JAUME','DIEGO','LUCAS','ANTHONY','ALEXIS','CARLOS'];
+let causas = [
+  "DESAFIO: TOQUES CON LA PALA DE PING PONG.",
+  "DESAFIO: LANZAMIENTO DE BOLAS DE PING PONG.",
+  "NORMA: QUIEN TIRE ESE MISMO COLOR BEBE (SOLO 1 RONDA).",
+  "NORMA: QUIEN TIRE EL MISMO NUMERO QUE HAY ABAJO BEBE (SOLO 1 RONDA).",
+  "NORMA: QUIEN TIRE LA CARTA MAS BAJA EN LA RONDA SIGUIENTE BEBE (EN LA SIGUIENTE RONDA).",
+  "NORMA: QUIEN SAQUE LA CARTA MAS BAJA DE ENTRE TODO BEBE.",
+  "DESAFIO: HACER UN 3 EN LINIA DE COLOR EN EL CUBO DE RUBIK.",
+  "NORMA: QUIEN TENGA 1 CARTA O QUIEN DIGA 'UNO' BEBE (SOLO 1 RONDA).",
+  "DESAFIO: CHOCAR LOS 5 CON LOS OJOS CERRADOS (HACER OTRA ITINERACION MAS PARA SACAR EL 2n NOMBRE)",
+  "DESAFIO: MINIJUEGO DE MARIO PARTY, QUIEN PIERDA BEBE.",
+  "DESAFIO: BEBE EL ULTIMO QUE SE TOQUE LA NARIZ.",
+  "DESAFIO GRUPAL DESBLOQUEADO: LOS DoD (DADOS DE LA MUERTE)."
+];
+let victima = "";
+let causa = "";
+
+
+
+// Funciones para la musica de fondo
+let mysong = document.getElementById("mysong");
+let icon = document.getElementById("icon");
+icon.onclick = function () {
+  if (mysong.paused) {
+    mysong.play();
+    icon.src = "img/play.png";
+  } else {
+    mysong.pause();
+    icon.src = "img/pause.png";
+  }
+};
+// funciones del PopUp /////////////////////////////////////////////////////////////////
+
+// Funciones para mostrar o cerrar PopUp
+function abrir(mostrar, ocultar) {
+  document.getElementById(mostrar).style.display = "flex";
+  document.getElementById(ocultar).style.display = "none";
+}
+function cerrar(ocultar, mostrar) {
+  document.getElementById(ocultar).style.display = "none";
+  document.getElementById(mostrar).style.display = "flex";
+}
+
+// Funciones añadir Victimas
+function pushnames(value) {
+  if ((inputnombres.value.length >= 4) & (inputnombres.value.length <= 12)) {
+    names.push(value);
+    mostrarnombres();
+    inputnombres.value = "";
+  } else {
+    inputnombres.value = "";
+    alert("Nombre no válido");
+  }
+}
+
+///ENTER/////
+document
+  .getElementById("inputnombres")
+  .addEventListener("keyup", function (event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+      document.getElementById("sub").click();
+    }
+  });
+
+// funcion Imprimir en popUp
+function mostrarnombres() {
+  let localizacion = document.getElementById("impresionarray");
+  let todolist = "";
+  names.forEach((elemento) => {
+    todolist += `<li>
+                            <button onclick='upDateName(upDate.value)' class="btnname" id= 'upDate' value='${elemento.toUpperCase()}'>${elemento.toUpperCase()}</button>
+                            <button onclick = "borrarVictimas(this)" class = "delete" id = "borrar">X</button>
+                         </li>`;
+  });
+  localizacion.innerHTML = todolist;
+}
+
+// Funcion borrar Victimas
+function borrarVictimas(elementothis) {
+  let victimaABorrar = elementothis.previousElementSibling.value;
+  let index = rangeArray(names, victimaABorrar);
+
+  names.splice(index, 1);
+
+  mostrarnombres();
+}
+
+
+//Funcion For Loop con filtro devuelve Indice
+function rangeArray(array, filter) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] == filter) {
+      return i;
+    }
+  }
+}
+//////////////////////////////////////////////////////////////////
+
+
+//funcion numero random
+function randomNum(maximo) {
+  let num = Math.random() * maximo;
+  let redondearNum = Math.floor(num);
+
+  return redondearNum;
+}
+
+//funcion elegir elemento de lista random
+function kill() {
+  victima = names.splice(randomNum(names.length), 1)[0];
+  causa = causas.splice(randomNum(causas.length), 1)[0];
+  console.log("causa: ", causa, "kill :", victima);
+}
+
+// funcion fecha random
+function diamuerte() {
+  let dia = Math.floor(Math.random() * 30) + 1;
+  let mes = Math.floor(Math.random() * 12) + 1;
+  let año = Math.floor(Math.random() * (2025 - 2021)) + 2021;
+  document.getElementById("fecha").innerHTML = `${dia} / ${mes} / ${año}`;
+  document.getElementById("victimafecha").innerHTML = `${dia} / ${mes} / ${año}`;
+}
+
+//funcion imprimir elementos en el DOOM
+function imprimirEnHoja() {
+  let localizacionCausa = document.querySelector("#causaMortal");
+  let localizacionVictima = document.querySelector("#victima");
+
+  localizacionCausa.innerHTML = causa.toUpperCase();
+  localizacionVictima.innerHTML = victima.toUpperCase();
+}
+
+// Funcion restablecer Arrays
+function resetAndClose() {
+  cerrar("containerDN", "inicio");
+  names = ['JAUME','DIEGO','LUCAS','ANTHONY','ALEXIS','CARLOS'];
+  causas = [
+    "DESAFIO: TOQUES CON LA PALA DE PING PONG.",
+    "DESAFIO: LANZAMIENTO DE BOLAS DE PING PONG.",
+    "NORMA: QUIEN TIRE ESE MISMO COLOR BEBE (SOLO 1 RONDA).",
+    "NORMA: QUIEN TIRE EL MISMO NUMERO QUE HAY ABAJO BEBE (SOLO 1 RONDA).",
+    "NORMA: QUIEN TIRE LA CARTA MAS BAJA EN LA RONDA SIGUIENTE BEBE (EN LA SIGUIENTE RONDA).",
+    "NORMA: QUIEN SAQUE LA CARTA MAS BAJA DE ENTRE TODO BEBE.",
+    "DESAFIO: HACER UN 3 EN LINIA DE COLOR EN EL CUBO DE RUBIK.",
+    "NORMA: QUIEN TENGA 1 CARTA O QUIEN DIGA 'UNO' BEBE (SOLO 1 RONDA).",
+    "DESAFIO: CHOCAR LOS 5 CON LOS OJOS CERRADOS (HACER OTRA ITINERACION MAS PARA SACAR EL 2n NOMBRE)",
+    "DESAFIO: MINIJUEGO DE MARIO PARTY, QUIEN PIERDA BEBE. (HACER ITINERACIONES PARA VER QUIENES JUEGAN",
+    "DESAFIO: BEBE EL ULTIMO QUE SE TOQUE LA NARIZ.",
+    "DESAFIO GRUPAL DESBLOQUEADO: LOS DoD (DADOS DE LA MUERTE)."
+  ];
+
+  mostrarnombres();
+}
+
+//funcion MAIN
+function start() {
+  if ((names.length != 0) & (causas.length != 0)) {
+    abrir("containerDN", "inicio");
+    diamuerte();
+    kill();
+    imprimirEnHoja();
+  }
+}
